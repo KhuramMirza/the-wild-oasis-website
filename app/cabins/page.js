c3146c2a3e43847128f216dc6d8732b125706f7a
Khuram Mirza <mirzakhuram101@gmail.com>
2025-06-23 16:55:52 +0500

    add metadata for pages

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,11 +1,14 @@
 import Counter from "@/app/_components/Counter";
 
+export const metadata = {
+  title: "Cabins",
+  description: "Explore our luxury cabins in the wild",
+};
+
 export default function Page() {
   return (
     <div>
       <h1>Cabins page</h1>
-
-      <Counter users={data} />
     </div>
   );
 }
