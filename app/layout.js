11332eb118a652aebf6eb3b6aa4d7120e806a4cd
Khuram Mirza <mirzakhuram101@gmail.com>
2025-06-24 23:18:51 +0500

    make the about, cabins, and homepage's basic layout

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,11 @@
-import Navigation from "@/app/_components/navigation";
-import Logo from "@/app/_components/Logo";
-
 import "@/app/_styles/globals.css";
+import Header from "./_components/Header";
+import { Josefin_Sans } from "next/font/google";
+
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export const metadata = {
   // title: "The Wild Oasis",
@@ -17,13 +21,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-primary-950 text-primary-100 min-h-screen">
-        <header>
-          <Logo />
-          <Navigation />
-        </header>
-        <main>{children}</main>
-        <footer>Copyright by The Wild Oasis</footer>
+      <body
+        className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen flex flex-col antialiased relative`}
+      >
+        <Header />
+        <div className="flex-1 px-8 py-12">
+          <main className="max-w-7xl mx-auto">{children}</main>
+        </div>
       </body>
     </html>
   );
