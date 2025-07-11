388bfe5aec363c7d083222d8ccdaa1ca6a12a6af
Khuram Mirza <mirzakhuram101@gmail.com>
2025-07-11 12:04:47 +0500

    make the server action for deleting the guest reservation, also use useTransition to monitor the state in button and revalidate the data after mutation in the deleteReservation server action function

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -3,11 +3,11 @@
 import { revalidatePath } from "next/cache";
 import { auth, signIn, signOut } from "./auth";
 import { supabase } from "./supabase";
+import { getBookings } from "./data-service";
 
 export async function updateGuest(formData) {
   const session = await auth();
-  if (!session)
-    throw new Error("You must be signed in to update your profile.");
+  if (!session) throw new Error("You must be logged in");
 
   const nationalID = formData.get("nationalID");
   const [nationality, countryFlag] = formData.get("nationality").split("%");
@@ -25,6 +25,25 @@ export async function updateGuest(formData) {
   if (error) throw new Error("Guest could not be updated");
 
   revalidatePath("/account/profile");
+}
+
+export async function deleteReservation(bookingId) {
+  const session = await auth();
+  if (!session) throw new Error("You must be logged in");
+
+  const guestBookings = await getBookings(session.user.guestId);
+  const guestBookingIds = guestBookings.map((booking) => booking.id);
+  if (!guestBookingIds.includes(bookingId))
+    throw new Error("You are not allowed to delete this booking");
+
+  const { error } = await supabase
+    .from("bookings")
+    .delete()
+    .eq("id", bookingId);
+
+  if (error) throw new Error("Booking could not be deleted");
+
+  revalidatePath("/account/reservations");
 }
 
 export async function signInAction() {
